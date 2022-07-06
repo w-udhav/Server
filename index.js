ea548408f03a85bd3a5cfac83685ea98c47782ad
w-udhav <95645604+w-udhav@users.noreply.github.com>
2022-07-06 19:48:04 +0530

    Test 1

--- index.js
+++ index.js
@@ -47,10 +47,10 @@ app.post("/send", function (req, res) {
 });
 
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
- console.log(`Server is running on port: ${port}`);
-});
+// const port = process.env.PORT || 4000;
+// app.listen(port, () => {
+//  console.log(`Server is running on port: ${port}`);
+// });
 
 
 
