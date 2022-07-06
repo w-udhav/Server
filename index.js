6e7ac3386d3803162d03b72990199f9a0774693e
w-udhav <95645604+w-udhav@users.noreply.github.com>
2022-07-06 12:13:06 +0530

    FInal commit

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post("/send", function (req, res) {
 
 
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
  console.log(`Server is running on port: ${port}`);
 });
