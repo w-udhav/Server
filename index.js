b9a281147cee8806d795c2cc0713e91f5fff0b98
w-udhav <95645604+w-udhav@users.noreply.github.com>
2022-07-06 22:01:41 +0530

    testing failed

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header('Access-Control-Allow-Methods','POST');
   next();
 });
 
