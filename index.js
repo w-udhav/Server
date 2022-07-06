c6618fa7f81878fb067a9fba283468d4618f33b4
w-udhav <95645604+w-udhav@users.noreply.github.com>
2022-07-06 22:06:35 +0530

    testing failed

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   res.header('Access-Control-Allow-Methods','POST');
+  res.header('Access-Control-Allow-Origin','https://localhost:3000')
   next();
 });
 
