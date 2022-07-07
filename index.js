6a405a01adf44c404a36edaf2748a980a1c1e3de
w-udhav <95645604+w-udhav@users.noreply.github.com>
2022-07-07 20:07:09 +0530

    2nd email setup

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     type: "OAuth2",
-    user: "ambrewster24@gmail.com",
+    user: "highhopestutoring.canningvale@gmail.com",
     pass: "",
-    clientId: "826262925552-1148ei1vit8ct556565pn8kgtfaesbmb.apps.googleusercontent.com",
-    clientSecret: "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",
-    refreshToken: "1//049TDRiGtrTlnCgYIARAAGAQSNgF-L9IrlulrUP-Y5ca1bptyPCIerUx1J67Og12_HklnfPvHPPP_bBYWNIlo8dkQQqRO4znzfw",
+    clientId: "1014401360433-kl2joalk5lomdsohpq8kva26tg8ke8qb.apps.googleusercontent.com",
+    clientSecret: "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",
+    refreshToken: "1//0454nWArMIPOdCgYIARAAGAQSNwF-L9Ir1zAp7yL2EOnV3o9R9HdfnymczrudNu8yXnwaYos6VEM-Asd8aLdOEdh1p2NGIIRxuzk",
   },
 });
 
@@ -33,15 +33,38 @@ app.post("/send", function (req, res) {
   const data = req.body
   console.log(data)
   let mailOptions = {
-    from: 'ambrewster24@gmail.com',
-    to: `${data.email}`,
-    subject: `${data.subject}`,
+    from: 'highhopestutoring.canningvale@gmail.com',
+    to: `highhopestutoring.canningvale@gmail.com`,
+    subject: `Assessment Form`,
     html:
       `
-        <div style='color:red; background-color:black; padding:20px'>
-          <p> Message : ${data.message} </p>
-        </div>
-        `
+      <div style='font-family:arial'>
+      <h1> Assessment &nbsp; Form </h1>
+
+      <div>
+          <h2 style='margin-top:70px; color:royalblue;'> Student Details: </h2>
+          <table style='border-bottom:1px solid black; min-width:30%;text-align:left'>
+              <tr>
+                  <th> Name:: </th>
+                  <td> ${data.name} </td>
+                  
+              </tr>
+              <tr>
+                  <th> Email:: </th>
+                  <td> ${data.email} </td>
+              </tr>
+              <tr>
+                  <th> Subject:: </th>
+                  <td> ${data.subject} </td>
+              </tr>
+              <tr>
+                  <th> Mesage:: </th>
+                  <td> ${data.message} </td>
+              </tr>
+          </table>
+      </div>
+  </div>
+      `
   }
 
   transporter.sendMail(mailOptions, function (err, data) {
@@ -53,6 +76,134 @@ app.post("/send", function (req, res) {
     }
   });
 });
+
+// Enrollment Form
+app.post("/sendform", function (req, res) {
+  const data = req.body
+  console.log(data)
+  let mailOptions = {
+    from: 'highhopestutoring.canningvale@gmail.com',
+    to: `highhopestutoring.canningvale@gmail.com`,
+    subject: `Enrolment Form`,
+    html:
+      `
+      <div style='font-family:arial'>
+        <h1> Assessment Form </h1>
+
+          <div>
+            <h2 style='margin-top:70px; color:royalblue;'> Student Details: </h2>
+            <table style='border-bottom:1px solid black; width:80%;text-align:left'>
+                <tr>
+                    <th> Name: </th>
+                    <td> ${data.firstName} &nbsp; ${data.lastName}</td> 
+                    <th> Date of Birth: </th>
+                    <td> ${data.dob} </td>
+                </tr>
+                <tr>
+                    <th> Gender: </th>
+                    <td> ${data.gender} </td>
+                    <th> School Name: </th>
+                    <td> ${data.schoolName} </td>
+                </tr>
+                <tr>
+                    <th> Year: </th>
+                    <td> ${data.year} </td>
+                    <th> Home Address: </th>
+                    <td> ${data.homeAdd} </td>
+                </tr>
+                <tr>
+                    <th> Suburb: </th>
+                    <td> ${data.subUrb} </td>
+                    <th> Postcode: </th>
+                    <td> ${data.postCode} </td>
+                </tr>
+                <tr>
+                    <th> Student Email: </th>
+                    <td> ${data.studentEmail} </td>
+                </tr>
+            </table>
+
+            <h2 style='margin-top:70px; color:royalblue;'> Parents Details: </h2>
+            <table style='border-bottom:1px solid black; width:80%; text-align:left'>
+                <tr>
+                    <th> Name 1: </th>
+                    <td> ${data.parentName1} </td>
+                    <th> Relation 1: </th>
+                    <td> ${data.relation1} </td>
+                </tr>
+                <tr>
+                    <th> Email 1: </th>
+                    <td> ${data.parentEmail1} 1 </td>
+                    <th> Contact: </th>
+                    <td> ${data.parentCont1} </td>
+                </tr>
+                <tr>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <th> Name 2: </th>
+                    <td> ${data.parentName2} </td>
+                    <th> Relation 2: </th>
+                    <td> ${data.relation2} </td>
+                </tr>
+                <tr>
+                    <th> Email 2: </th>
+                    <td> ${data.parentEmail2} </td>
+                    <th> Contact 2: </th>
+                    <td> ${data.parentCont2} </td>
+                </tr>
+            </table>
+
+            <h2 style='margin-top:70px; color:royalblue;'> Authorized Person Details: </h2>
+            <table style='border-bottom:1px solid black; width:80%; text-align:left'>
+                <tr>
+                    <th> Name 1: </th>
+                    <td> ${data.authPerson1} </td>
+                    <th> Contact 1: </th>
+                    <td> ${data.authCont1} </td>
+                </tr>
+                <tr>
+                    <th> Relation 1: </th>
+                    <td> ${data.authRelation1} </td>
+                </tr>
+                <tr>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <th> Name 2: </th>
+                    <td> ${data.authPerson2} </td>
+                    <th> Contact 2: </th>
+                    <td> ${data.authCont2} </td>
+                </tr>
+                <tr>
+                    <th> Relation 2: </th>
+                    <td> ${data.authRelation2} </td>
+                </tr>
+            </table>
+
+            <h2 style='margin-top:70px; color:royalblue;'> Health Details: </h2>
+            <p>1. Is your child allergic to any food? <span> <b> ${data.foodAllergy} </b> </span> </p>
+            <p>2. Is your child on medication? <span> <b> ${data.medication} </b> </span> </p>
+            <p>3. Does your child allergic to any medications? <span> <b> ${data.medAllergy} </b> </span> </p>
+            <p>4. Does your child suffer from any health problem? <span> <b> ${data.healthProb} </b> </span> </p>
+          </div>
+        
+          <h4 style='margin-top:70px; color:royalblue;'> Signature : <span style='color:black'> ${data.sign} </span> </h4>
+      </div>
+      `
+  }
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error" + err);
+    } else {
+      console.log("Email sent successfully");
+      // console.log(res.status)
+    }
+  });
+});
+
+
 
 
 const port = process.env.PORT || 4000;
